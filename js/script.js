b670be9595b010298e2baf098b9eeaea6050d9e2
TasfiaZaima <tasfia.zaima000@gmail.com>
2025-01-27 14:18:47 +0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,128 @@
+
+
+
+$(".fa-solid").click(function(){
+    $(".n").slideToggle()
+})
+
+// $(".button").click(function(){
+//     $(".n").slideUp()
+// })
+
+
+// $(".fa-solid").click(function(){
+//     $(".fa-solid").toggleClass("rotate")  ; 
+//    })
+// $(".rotate").click(function () {
+//     $(this).toggleClass("down");
+// })
+
+
+
+
+
+
+
+// $(".bar").click(function(){
+//     $(".items").fadeToggle()
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".banner-slider").slick({
+//     arrows: false,
+//     dots : true,
+    
+// })
+
+// $(".img-slider").slick({
+//     arrows: false,
+//     dots : true,
+//     slidesToShow: 5,
+// })
+
+// $(".text-slider").slick({
+//     arrows: false,
+//     dots : false,
+//     slidesToShow: 1,
+//     asNavFor:".feedback-img-slider",
+// })
+
+// // $(".feedback-slider").slick({
+// //     arrows: false,
+// //     dots : true,
+
+// // })
+
+
+// $(".feedback-img-slider").slick({
+//     arrows: false,
+//     dots : false,
+//     slidesToShow: 3,
+//     centerMode: true,
+//     centerPadding:"0",
+//     asNavFor:".text-slider",
+// })
+
+
+
+
+
+// $('.venobox').venobox();
+
+
+
+
+
+
